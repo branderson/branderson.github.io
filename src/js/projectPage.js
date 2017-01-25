3e94420e62ced63c56c058360f151ecf55f7329b
Brad Anderson <brad.anderson1995@gmail.com>
2017-01-25 00:17:23 -0600

    Add skeleton for Lit Limo page

--- src/js/projectPage.js
+++ src/js/projectPage.js
@@ -3,3 +3,9 @@ $('#foodFeudButton').on('click', function(event) {
     $('#foodFeudFrame').attr('src', 'https://itch.io/embed-upload/364731?color=272C2D');
     $(this).hide();
 });
+
+$('#litLimoButton').on('click', function(event) {
+    event.preventDefault();
+    $('#litLimoFrame').attr('src', 'https://itch.io/embed-upload/102358?color=272C2D');
+    $(this).hide();
+});
